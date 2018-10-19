1fa0201ab055edcba2e7e3eda397862bd21fe649
Angelo Lim <angelovinolim@gmail.com>
2018-10-18 22:11:13 -0400

    ,;'/=-\]

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner kbInput = new Scanner(System.in);
+        String sentence;
+        String sp [];
+        int i = 0;
+
+        while (i == 0) {
+            System.out.println("Please enter in a sentence.");
+            sentence = kbInput.nextLine().toUpperCase() + "X";
+            if (sentence.equals("EXITX")) {
+                i = 1;
+            }
+            if (i == 0) {
+                sp = sentence.split("S\\s*A");
+                int occurrences = sp.length - 1;
+                System.out.println("There are " + occurrences + " occurrences.\n");
+            }
+        }
     }
 }
